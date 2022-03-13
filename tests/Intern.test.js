c465c22bb2c9d76ae3ea98266843c3d91baad59f
unknown <Sophia Nguyen>
2022-03-13 10:00:25 -0500

    updated variables in intern-test

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -2,9 +2,9 @@ const Intern = require("../lib/Intern");
 
 test("creates an intern object", () => {
     const intern = new Intern("Matt", 66, "matt@email.com");
-    expect(employee.name).toBe("Matt");
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toEqual(expect.any(String));
+    expect(intern.name).toBe("Matt");
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toEqual(expect.any(String));
 });
 
 test("get intern name", () => {
