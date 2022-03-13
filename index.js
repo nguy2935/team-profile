7acc00ed12718153a551f7150d037fcddbf2c71b
unknown <Sophia Nguyen>
2022-03-13 14:42:00 -0500

    created index.html and style.css to display team profile

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // rqeuire node modules to install
 const inquirer = require("inquirer");
 const fs = require("fs");
-const path = require("path");
 
 // team profiles to generate
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
-const Inern = require("./lib/Intern");
+const Intern = require("./lib/Intern");
 
 const pageRender = require("./src/pageRender");
 
